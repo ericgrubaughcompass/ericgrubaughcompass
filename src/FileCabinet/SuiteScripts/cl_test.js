3ae4ad04789cb2d3bcae5507e9d12a6aa109771a
eric.grubaugh@compass.com <eric.grubaugh@compass.com>
2023-05-08 11:02:16 -0600

    Added client script for validating eslint config; added eslint config file and scripts

---
+++ src/FileCabinet/SuiteScripts/cl_test.js
@@ -0,0 +1,219 @@
+/**
+ * Module Description...
+ *
+ * @type {Object} module-name
+ *
+ * @copyright 2023 Compass
+ * @author Eric Grubaugh <eric.grubaugh@compass.com>
+ *
+ * @NApiVersion 2.1
+ * @NModuleScope XXX
+
+ * @NScriptType ClientScript
+ */
+define([], () => {
+  /**
+   * pageInit event handler; executed when the page completes loading or when the form is reset.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {string} context.mode - The access mode of the current record.
+   * @param {CurrentRecord} context.currentRecord - The record in context
+   */
+  function pageInit(context) {
+    // TODO
+  }
+
+  /**
+   * validateField event handler; executes when a field is about to be changed by a user or
+   * client side call.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current record the user is manipulating
+   *  in the UI
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   * @param {string} context.fieldId - The internal ID of the field being validated.
+   * @param {string} [context.lineNum] - The index of the line if the field is in a sublist or
+   *  matrix.
+   * @param {string} [context.columnNum] - The index of the column if the field is in a matrix.
+   *
+   * @return {boolean} true if the field is valid; false to prevent the field value from changing.
+   */
+  function validateField(context) {
+    // TODO
+    return true;
+  }
+
+  /**
+   * fieldChanged event handler; executed when a field is changed by a user or client side call.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   * @param {string} context.fieldId - The internal ID of the field that was changed.
+   * @param {string} [context.lineNum] - The index of the line if the field is in a sublist or
+   *      matrix.
+   * @param {string} [context.columnNum] - The index of the column if the field is in a matrix.
+   */
+  function fieldChanged(context) {
+    // TODO
+  }
+
+  /**
+   * executed when the record enters the localization context that is specified on the script
+   * deployment record
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record
+   * @param {string} context.locale - The list of countries that represent the new localization
+   *      context.
+   */
+  function localizationContextEnter(context) {
+    // TODO
+  }
+
+  /**
+   * executed when the record exits the localization context that is specified on the script
+   * deployment record
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record
+   * @param {string} context.locale - The list of countries that represent the new localization
+   *      context.
+   */
+  function localizationContextExit(context) {
+    // TODO
+  }
+
+  /**
+   * postSourcing event handler; executed on transaction forms when a field that sources
+   * information from another field is modified.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   * @param {string} context.fieldId - The internal ID of the field that triggered postSourcing.
+   */
+  function postSourcing(context) {
+    // TODO
+  }
+
+  /**
+   * lineInit event handler; executed when an existing line is selected.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   *
+   * @param {CurrentRecord} context.currentRecord - The current form record
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   */
+  function lineInit(context) {
+    // TODO
+  }
+
+  /**
+   * validateLine event handler; executed before a line is added to an inline editor sublist or
+   * editor sublist.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record.
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   *
+   * @return {boolean} true to allow line addition; false to prevent line addition.
+   */
+  function validateLine(context) {
+    // TODO
+    return true;
+  }
+
+  /**
+   * validateInsert event handler; executed when you insert a line into an edit sublist.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record.
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   *
+   * @return {boolean} true if the line can be inserted; false to prevent the line insertion.
+   */
+  function validateInsert(context) {
+    // TODO
+    return true;
+  }
+
+  /**
+   * validateDelete event handler; executed when removing an existing line from an edit sublist.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current record the user is manipulating
+   *      in the UI
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   *
+   * @return {boolean} true to allow the removal; false to prevent the removal.
+   */
+  function validateDelete(context) {
+    // TODO
+    return true;
+  }
+
+  /**
+   * sublistChanged event handler; executed after a sublist is inserted, removed, or edited.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The current form record.
+   * @param {string} context.sublistId - The internal ID of the sublist.
+   */
+  function sublistChanged(context) {
+    // TODO
+  }
+
+  /**
+   * saveRecord event handler; executed after the submit button is pressed but before the form is
+   * submitted.
+   *
+   * @gov XXX
+   *
+   * @param {Object} context
+   * @param {CurrentRecord} context.currentRecord - The record in context
+   *
+   * @return {boolean} true if the record is valid; false to stop form submission.
+   */
+  function saveRecord(context) {
+    // TODO
+    return true;
+  }
+
+  return {
+    fieldChanged,
+    lineInit,
+    localizationContextEnter,
+    localizationContextExit,
+    pageInit,
+    postSourcing,
+    saveRecord,
+    sublistChanged,
+    validateDelete,
+    validateField,
+    validateInsert,
+    validateLine
+  };
+});
