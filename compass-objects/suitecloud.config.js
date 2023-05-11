b6d97e2341bbde3a905e7f52b81f30d8b74d8856
eric.grubaugh@compass.com <eric.grubaugh@compass.com>
2023-05-11 14:08:48 -0600

    Removed sdf-base config files for now

--- compass-objects/suitecloud.config.js
+++ compass-objects/suitecloud.config.js
@@ -1,15 +1,18 @@
-const SuiteCloudJestUnitTestRunner = require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner');
-
 module.exports = {
-	defaultProjectFolder: 'src',
-	commands: {
-		"project:deploy": {
-			beforeExecuting: async args => {
-				await SuiteCloudJestUnitTestRunner.run({
-				    // Jest configuration options.
-				});
-				return args;
-			},
-		},
-	},
+  defaultProjectFolder: 'src',
+  commands: {
+    'project:deploy': {
+      beforeExecuting: (options) => {
+        options.arguments.accountspecificvalues = 'WARNING';
+        return options;
+      }
+    },
+    'project:validate': {
+      beforeExecuting: (options) => {
+        options.arguments.accountspecificvalues = 'WARNING';
+        options.arguments.server = true;
+        return options;
+      }
+    }
+  }
 };
